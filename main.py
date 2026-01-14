a28f2d4da7bf88b34c653519df1939f0801b6d07
Simon Cossar <scossar3@gmail.com>
2026-01-14 14:32:44 -0800

    cleanup

--- main.py
+++ main.py
@@ -270,9 +270,3 @@ RETURNING id
 
 embeddings_generator = EmbeddingGenerator()
 embeddings_generator.generate_embeddings()
-
-# chroma data
-# rsync -avz --delete /home/scossar/projects/python/embeddings_generator/chroma/ scossar@zalgorithm.com:/tmp/chroma-data/
-
-# sqlite database
-# rsync -avz --delete /home/scossar/projects/python/embeddings_generator/sqlite/sections.db scossar@zalgorithm.com:/tmp/sections.db
